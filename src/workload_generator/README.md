d5839c9394c3dbca7ba7d31971bd25fc897801c4
sylvesterkwon <sylvesterkwon@gmail.com>
2026-04-06 15:29:03 +0900

    misc: Remove legacy workload_generator

--- src/workload_generator/README.md
+++
@@ -1,2 +0,0 @@
-## Workload Generator for Secondary Index
-- [ ] TODO: Add instruction for workload generator
--- src/workload_generator/distribution_generator.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <cstdint>
-
-// Interface for Distribution Generator
-class DistributionGenerator {
-public:
-  virtual ~DistributionGenerator() = default;
-  virtual uint64_t Next() = 0;
-};
